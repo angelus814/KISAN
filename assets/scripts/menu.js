73ad510817a60f4b988bcdece05f3111993aeb47
Mostro Papi <ruizvaria814@gmail.com>
2024-07-31 12:50:59 -0500

    commits

---
+++ assets/scripts/menu.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".headerMenu")
+let nav = document.getElementById("nav")
+menu.addEventListener("click", ()=>{
+    nav.classList.toggle("menuAbierto")
+    console.log("rey")
+})
